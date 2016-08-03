394ef3a26e771b1995166d094dc201d3391fe472
Anton <antonfh@gmail.com>
2016-08-03 10:10:15 +0200

    About us

--- views/site/about.php
+++ views/site/about.php
@@ -10,9 +10,16 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <h3>Why use songs in the primary classroom?</h3>
     <p>
-        This is the web site for JungoJingle!
-        We are still under construction.
+
+        Songs tend to repetitive and have a strong rhythm. They are easily learnt by primary children and quickly become favourites because of their familiarity. They are fun and motivating for children. They allow language to be reinforced in a natural context, both with structures and vocabulary.
+</p>
+    <p>
+        All songs build confidence in young learners and even shy children will enjoy singing or acting out a song as part of a group or whole class. This also develops a sense of class identity. Children are often proud of what they have learnt and will like having the opportunity to ‘show off' what they have learnt to friends or family.
+</p>
+    <p>
+        Many songs can help develop memory and concentration, as well as physical co-ordination, for example when doing the actions for a song. For the teacher, songs can be a wonderful starting point for a topic and can fit in well with topics, skills, language and cross-curricular work.
     </p>
 
 </div>
