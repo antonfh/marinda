d8c15cf1b20144f875f2b1580f209830274ae989
Anton Heuschen <anton@pdmssa.com>
2016-08-04 19:16:19 +0200

    About

--- views/site/about.php
+++ views/site/about.php
@@ -12,20 +12,20 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h3>We develop kids through music and activity</h3>
 
-    <p>Our focus on the development from a young age. Our Aim:</p>
+    <p>Our focus is the development, from a young age, to learn a new language. Our Aim:</p>
     <ul>
         <li>
             Introducing English to toddlers age 3-5, through music, movement and fun activities.
         </li>
         <li>
-            Classes are split up into 3 catergories:
+            Classes are split up into 3 categories:
             <ol>
                 <li>
                     Music - Fun songs with lots of movement based around our monthly theme.
                 </li>
                 <li>
                     Activity/Create - 20 min of creating something, sometimes colouring and drawing and on other days
-                    just fin with games and words.
+                    just fun with games and words.
                 </li>
                 <li>
                     Read and Rhyme - We end our class with a rhyme and book of the month.
