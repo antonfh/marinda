87797614953944faa7ca85706cb138d065c0278b
Anton Heuschen <anton@pdmssa.com>
2016-08-04 20:08:09 +0200

    home

--- views/site/index.php
+++ views/site/index.php
@@ -18,7 +18,7 @@ $this->registerMetaTag([
 <div class="site-index">
 
     <div class="jumbotron">
-        <b class="bold_black">Welcome, to</b> <b class="bold_red">JungoJingle!</b>
+        <b class="bold_black">Welcome, to:
         <br />
         <img src="/img/logo/Logo_Jungo.jpg" width="80%" height="80%" class="jumbotron">
 
