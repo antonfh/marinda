d03b7d769bcc4da8d7ee67fc7a9908c9e0d8205f
Anton Heuschen <anton@pdmssa.com>
2016-08-04 22:19:13 +0200

    home

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -89,11 +89,12 @@ Icon::map($this);
     <div class="container">
         <p class="pull-left">&copy; JungoJingle <?= date('Y') ?></p>
 
-        <p class="pull-right"><a href="http://www.youtube.com/channel/UC0sBuwz-2dVTAQdF076NYOA">
+        <p class="pull-right"><a href="http://www.youtube.com/channel/UC0sBuwz-2dVTAQdF076NYOA" target="_blank">
                 <i class="fa fa-youtube-square fa-2x" aria-hidden="true"></i>
                 </a>
         </p>
-        <p class="pull-right"><a href="https://plus.google.com/+Jungojingle">
+        &nonbreakingspace;
+        <p class="pull-right"><a href="https://plus.google.com/+Jungojingle" target="_blank">
                 <i class="fa fa-google-plus-square fa-2x" aria-hidden="true"></i>
                 </i>
             </a>
