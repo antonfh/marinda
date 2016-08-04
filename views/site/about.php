7184f2653083d65a6b28d1b4bf210e908defd906
Anton Heuschen <anton@pdmssa.com>
2016-08-04 19:08:53 +0200

    About

--- views/site/about.php
+++ views/site/about.php
@@ -4,22 +4,52 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'About us';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <h3>We develop kids through music and activity</h3>
+
+    <p>Our focus on the development from a young age. Our Aim:</p>
+    <ul>
+        <li>
+            Introducing English to toddlers age 3-5, through music, movement and fun activities.
+        </li>
+        <li>
+            Classes are split up into 3 catergories:
+            <ol>
+                <li>
+                    Music - Fun songs with lots of movement based around our monthly theme.
+                </li>
+                <li>
+                    Activity/Create - 20 min of creating something, sometimes colouring and drawing and on other days
+                    just fin with games and words.
+                </li>
+                <li>
+                    Read and Rhyme - We end our class with a rhyme and book of the month.
+                </li>
+            </ol>
+        </li>
+    </ul>
+
+
     <h3>Why use songs in the primary classroom?</h3>
     <p>
-
-        Songs tend to repetitive and have a strong rhythm. They are easily learnt by primary children and quickly become favourites because of their familiarity. They are fun and motivating for children. They allow language to be reinforced in a natural context, both with structures and vocabulary.
-</p>
+        Songs tend to repetitive and have a strong rhythm. They are easily learnt by primary children and quickly become
+        favourites because of their familiarity. They are fun and motivating for children. They allow language to be
+        reinforced in a natural context, both with structures and vocabulary.
+    </p>
     <p>
-        All songs build confidence in young learners and even shy children will enjoy singing or acting out a song as part of a group or whole class. This also develops a sense of class identity. Children are often proud of what they have learnt and will like having the opportunity to ‘show off' what they have learnt to friends or family.
-</p>
+        All songs build confidence in young learners and even shy children will enjoy singing or acting out a song as
+        part of a group or whole class. This also develops a sense of class identity. Children are often proud of what
+        they have learnt and will like having the opportunity to ‘show off' what they have learnt to friends or family.
+    </p>
     <p>
-        Many songs can help develop memory and concentration, as well as physical co-ordination, for example when doing the actions for a song. For the teacher, songs can be a wonderful starting point for a topic and can fit in well with topics, skills, language and cross-curricular work.
+        Many songs can help develop memory and concentration, as well as physical co-ordination, for example when doing
+        the actions for a song. For the teacher, songs can be a wonderful starting point for a topic and can fit in well
+        with topics, skills, language and cross-curricular work.
     </p>
 
 </div>
